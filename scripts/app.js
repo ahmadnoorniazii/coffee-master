18441964c732d74b8035eb86ebcaf4bd56b1bae1
Ahmad Noor <a.noor@upsidecode.de>
2024-10-02 05:38:06 +0200

    requested for storage persist using navigator.storage

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,24 @@ import Menu from './Menu.js';
 import Order from './Order.js';
 import Router from './Router.js';
 
+(async () => {
+    if(navigator?.storage?.persist){
+        if(!await navigator.storage.persisted()){
+            const result = await navigator.storage.persist();
+            console.log(`Was Persistent Storage Request granted? ${result}`);
+        } else {
+            console.log(`Persistent Storage already granted`)
+        }
+    }
+})();
+
+(async function (){
+    if(navigator?.storage?.estimate){
+        const q = await navigator.storage.estimate();
+        console.log(`Estimate Quota available: ${q.quota/1024/1024}MB`);
+    }
+})()
+
 window.addEventListener("DOMContentLoaded", () => {
     Router.init();
     Menu.load();
